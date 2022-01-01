b88652115f262c0969a876b3a871c84a9564638c
eslam <eslam@DESKTOP-0B4VRIP>
2022-01-01 14:13:14 -0800

    adding the jquery lib

---
+++ wwwroot/js/index.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("Hello Pluralsight");
+
+
+
+    var theForm = $("#theForm");
+    /*theForm.hide();*/
+
+    function foo() {
+
+    };
+
+    var button = $("buyButton");
+    button.on("click", function () {
+        alert("Buying Item");
+    });
+
+    var productInfo = $(".product-props");
+
+    productInfo.on("click", function () {
+        console.log("You Clicked On " + $(this).text());
+    });
+
+
+
+});

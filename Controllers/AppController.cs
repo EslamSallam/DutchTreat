3a65ac97fb4d93f0d23b3db88021ca9c883451c0
EslamSallam <eslam.salam1369@gmail.com>
2022-01-07 17:02:17 -0800

    Adding a sample code for Mail Service

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace DutchTreat.Controllers
 {
@@ -24,7 +26,27 @@ namespace DutchTreat.Controllers
         {
             if (ModelState.IsValid)
             {
+                MimeMessage message = new MimeMessage();
 
+                MailboxAddress from = new MailboxAddress("Admin",
+                "e.sallam@softexsw.com");
+                message.From.Add(from);
+
+                MailboxAddress to = new MailboxAddress("User",
+                "eslam.salam1369@gmail.com");
+                message.To.Add(to);
+
+                message.Subject = "Test Email About MailKit";
+
+                BodyBuilder bodyBuilder = new BodyBuilder();
+                bodyBuilder.HtmlBody = "<h1>Hello World!</h1>";
+                bodyBuilder.TextBody = "Hello World!";
+
+                message.Body = bodyBuilder.ToMessageBody();
+
+                SmtpClient client = new SmtpClient();
+                client.Connect("smtp_address_here", port_here, true);
+                client.Authenticate("e.sallam@softexsw.com", "pwd_here");
             }
             else
             {

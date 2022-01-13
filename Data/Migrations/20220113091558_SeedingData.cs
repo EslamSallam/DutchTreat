779085f520e035a27f3e445ab0899bc7bd7953ba
ESallam <e.sallam@softexsw.com>
2022-01-13 12:59:53 +0200

    Lts For Seeding Data

---
+++ Data/Migrations/20220113091558_SeedingData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2022, 1, 13, 9, 15, 58, 47, DateTimeKind.Utc).AddTicks(5847), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}

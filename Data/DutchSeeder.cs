0951657396740658052d778927f60d78f1e68f3b
ESallam <96535069+ESallam@users.noreply.github.com>
2022-01-22 00:54:31 +0200

    creating the seeder class

---
+++ Data/DutchSeeder.cs
@@ -0,0 +1,51 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchSeeder
+    {
+        private readonly DutchContext _ctx;
+        private IWebHostEnvironment _env;
+
+        public DutchSeeder(DutchContext ctx, IWebHostEnvironment env)
+        {
+            _ctx = ctx;
+            _env = env;
+        }
+
+        public void Seed()
+        {
+            _ctx.Database.EnsureCreated();
+            if (!_ctx.products.Any())
+            {
+                var filePath = Path.Combine(_env.ContentRootPath, "Data/art.json");
+                var json = File.ReadAllText(filePath);
+                var products = JsonSerializer.Deserialize<IEnumerable<Product>>(json);
+                _ctx.products.AddRange(products);
+
+                var order = new Order()
+                {
+                    OrderDate = DateTime.Now,
+                    OrderNumber = "10000",
+                    Items = new List<OrderItem>()
+                    {
+                        new OrderItem()
+                        {
+                            Product = products.First(),
+                            Quantity = 5,
+                            UnitPrice = products.First().Price
+                        }
+                    }
+                };
+
+            }
+        }
+    }
+}
